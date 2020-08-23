42585731d0853ae34ac5952bde1eadc3ea3efe53
Ashraful <srabon444@gmail.com>
2020-08-23 06:44:56 +0600

    Variable source code

---
+++ Tim Buchalka/Variables/src/varibale.java
@@ -0,0 +1,12 @@
+public class varibale {
+    public static void main(String[] args) {
+        int variableX = (10 + 5) + (2 * 5);
+        int variableY = 12;
+        int variableZ = 6;
+        int myTotal = variableX + variableY + variableZ;
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+}
