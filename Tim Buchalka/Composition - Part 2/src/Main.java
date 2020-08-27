badc2058a61cf0d03de13e08f4494b735d50b18e
Ashraful <srabon444@gmail.com>
2020-08-27 11:46:39 +0600

    Part of Tim Buchalka Course Learning

---
+++ Tim Buchalka/Composition - Part 2/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "Dell", "240W", dimensions);
+
+        Monitor theMonitor = new Monitor("ROG Monitor", "Asus", 27, new Resolution(1920, 1080));
+
+        MotherBoard motherBoard = new MotherBoard("BJ-200", "Asus", 4, 6, "V2.54");
+
+        PC thePC = new PC(theCase, theMonitor, motherBoard);
+        thePC.powerUp();
+    }
+}
