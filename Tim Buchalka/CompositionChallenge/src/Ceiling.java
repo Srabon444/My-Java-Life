e78171542cbf6acb54a7bbc6015f3f8c66830dca
Ashraful <srabon444@gmail.com>
2020-08-27 12:22:35 +0600

    Part of Tim Buchalka Course Learning

---
+++ Tim Buchalka/CompositionChallenge/src/Ceiling.java
@@ -0,0 +1,17 @@
+public class Ceiling {
+    private int height;
+    private int printedColour;
+
+    public Ceiling(int height, int printedColour) {
+        this.height = height;
+        this.printedColour = printedColour;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPrintedColour() {
+        return printedColour;
+    }
+}
